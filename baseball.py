c69c24928106d005c1c7ea820ffc02a71cb82f14
Geoffrey Sechter <geoffrey.sechter@gmail.com>
2015-06-11 08:31:46 -0400

    adding twilio and yankees logic

--- baseball.py
+++ baseball.py
@@ -1,126 +1,64 @@
+from sys import argv
+from os import environ
 from datetime import datetime, timedelta
 import requests
-import json
 
-#function to determine the status of a game, if no team selected
-#based on the progress, different data is returned
+from twilio.rest import TwilioRestClient
 
-def game_info():
-    if game['status']['status'] == "In Progress":
-        return '%s (%s) vs %s (%s) @ %s %s' % (
-                game['away_team_name'],
-                game['linescore']['r']['away'],
-                game['home_team_name'],
-                game['linescore']['r']['home'],
-                game['venue'],
-                game['status']['status']
-            )
-    elif (game['status']['status'] == "Final" or game['status']['status'] == "Game Over"):
-        return '%s (%s) vs %s (%s) @ %s %s' % (
-                game['away_team_name'],
-                game['linescore']['r']['away'],
-                game['home_team_name'],
-                game['linescore']['r']['home'],
-                game['venue'],
-                game['status']['status']
-            )
-    elif (game['status']['status'] == "Pre-Game" or game['status']['status'] == "Preview"):
-        return '%s vs %s @ %s %s%s %s' % (
-                game['away_team_name'],
-                game['home_team_name'],
-                game['venue'],
-                game['home_time'],
-                game['hm_lg_ampm'],
-                game['status']['status']
-            )
 
-#function to determine the status of a game, if a team is selected
-#based on the progress, different data is returned
+TWILIO_ID = environ.get('TWILIO_YANKEES_ID')
+TWILIO_TOKEN = environ.get('TWILIO_YANKEES_TOKEN')
+TWILIO_NUMBER = environ.get('TWILIO_YANKEES_NUMBER')
 
-def team_score():
-    if game['status']['status'] == "In Progress":
-        return \
-        '-------------------------------\n' \
-        '%s (%s) vs. %s (%s) @ %s\n' \
-        '%s: %s of the %s\n' \
-        'Pitching: %s || Batting: %s || S: %s B: %s O: %s\n' \
-        '-------------------------------' % (
-                game['away_team_name'],
-                game['linescore']['r']['away'],
-                game['home_team_name'],
-                game['linescore']['r']['home'],
-                game['venue'],
-                game['status']['status'],
-                game['status']['inning_state'],
-                game['status']['inning'],
-                game['pitcher']['last'],
-                game['batter']['last'],
-                game['status']['s'],
-                game['status']['b'],
-                game['status']['o']
-            )
-    elif (game['status']['status'] == "Final" or game['status']['status'] == "Game Over"):
-        return \
-        '-------------------------------\n' \
-        '%s (%s) vs. %s (%s) @ %s\n' \
-        'W: %s || L: %s || SV: %s\n' \
-        '-------------------------------' % (
-                game['away_team_name'],
-                game['linescore']['r']['away'],
-                game['home_team_name'],
-                game['linescore']['r']['home'],
-                game['venue'],
-                game['winning_pitcher']['name_display_roster'],
-                game['losing_pitcher']['name_display_roster'],
-                game['save_pitcher']['name_display_roster']
-            )
-    elif (game['status']['status'] == "Pre-Game" or game['status']['status'] == "Preview"):
-        return \
-        '-------------------------------\n' \
-        '%s vs %s @ %s %s%s\n' \
-        'P: %s || P: %s\n' \
-        '-------------------------------' % (
-                game['away_team_name'],
-                game['home_team_name'],
-                game['venue'],
-                game['home_time'],
-                game['hm_lg_ampm'],
-                game['away_probable_pitcher']['name_display_roster'],
-                game['home_probable_pitcher']['name_display_roster']
-               )
 
-#function to determine which feed to grab based on user input
-
-def date_url(date):
-    if date == "yesterday":
-        baseball_url = "http://gd2.mlb.com/components/game/mlb/year_%d/month_%s/day_%s/master_scoreboard.json" \
-        % (now.year, now.strftime("%m"), yesterday.strftime("%d"))
-    else:
-        baseball_url = "http://gd2.mlb.com/components/game/mlb/year_%d/month_%s/day_%s/master_scoreboard.json" \
-                % (now.year, now.strftime("%m"), now.strftime("%d"))
+def get_game_url(game_date):
+    baseball_url = "http://gd2.mlb.com/components/game/mlb/year_{year}/month_{month}/day_{day}/master_scoreboard.json".format(
+        year=game_date.year,
+        month=game_date.strftime("%m"),
+        day=game_date.strftime("%d")
+        )
     return baseball_url
 
-#process to set date, and convert if need be.
 
-now = datetime.now()
-yesterday = datetime.now() - timedelta(days=1)
-date = raw_input('Today or yesterday? ').lower()
+def get_game_data():
+    """
+    Add date argument at some point
+    """
+    game_date = datetime.now() - timedelta(days=1)
+    request_url = get_game_url(game_date)
+    baseball_data = requests.get(request_url)
+    game_data = baseball_data.json()
+    game_array = game_data['data']['games']['game']
+    return game_array
 
-#builds the data structure from the feed
 
-baseball_data = requests.get(date_url(date))
-game_data = baseball_data.json()
-game_array = game_data['data']['games']['game']
+def find_winner(team, game_data):
+    game_score = [{g['home_name_abbrev']:g['linescore']['r']['home'], g['away_name_abbrev']:g['linescore']['r']['away']} for g in game_data if team in (g['home_name_abbrev'] or g['away_name_abbrev'])]
 
-team = raw_input('What team do you want? ex. SF or NYY ').upper()
-
-for game in game_array:
-    if team == "":
-        disp = game_info()
-        if disp:
-            print disp
+    runs_scored = game_score[0][team]
+    if runs_scored is max(game_score[0].values()) and runs_scored >= 6:
+        message = "Yankees won! Use YANKEES6 at www.papajohns.com for 50% off! :-)" 
     else:
-        if (game['home_name_abbrev']) == team or (game['away_name_abbrev']) == team:
-            disp = team_score()
-            if disp:
-                print disp
+        message = "No luck today, Yankees lost."
+    return message
+
+
+def send_message(message, to_number):
+    client = TwilioRestClient(TWILIO_ID, TWILIO_TOKEN)
+    text_message = client.messages.create(
+        body=message,
+        to=to_number,
+        from_=TWILIO_NUMBER
+        )
+    print(message)
+
+
+if __name__ == '__main__':
+    if len(argv) > 2:
+        team_name = argv[1]
+        game_data = get_game_data()
+        winner = find_winner(team_name, game_data)
+        to_number = argv[2]
+        send_message(winner, to_number)
+    else:
+        raise Exception("You must pass an abbreviated team name and phone number as arguments")
